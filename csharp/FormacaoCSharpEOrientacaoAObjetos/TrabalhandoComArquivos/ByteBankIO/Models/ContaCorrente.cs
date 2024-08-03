96a04539dac1e1fce6a38f80522b1846518d30a0
Matheus Colombo da Cruz <matheus.colombo@autoglass.com.br>
2024-08-03 18:01:02 -0300

    Atualizado curso universidade dos dados

--- csharp/FormacaoCSharpEOrientacaoAObjetos/TrabalhandoComArquivos/ByteBankIO/Models/ContaCorrente.cs
+++ csharp/FormacaoCSharpEOrientacaoAObjetos/TrabalhandoComArquivos/ByteBankIO/Models/ContaCorrente.cs
@@ -1,4 +1,4 @@
-namespace ByteBankIO
+namespace ByteBankIO.Models
 
 {
     public class ContaCorrente
--- csharp/FormacaoCSharpEOrientacaoAObjetos/TrabalhandoComArquivos/ByteBankIO/Program.cs
+++ csharp/FormacaoCSharpEOrientacaoAObjetos/TrabalhandoComArquivos/ByteBankIO/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ByteBankIO.Models;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string caminhoArquivo = "contas.txt";
+        int numeroDeBytesLidos = -1;
+        var buffer = new byte[1024]; //1kb
+
+        var fluxoArquivo = new FileStream(caminhoArquivo, FileMode.Open);
+        
+        while(numeroDeBytesLidos != 0) {
+            numeroDeBytesLidos = fluxoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+        }
+
+    }
+
+    public static void EscreverBuffer(byte[] buffer)
+    {
+        foreach (var meuByte in buffer)
+        {
+            Console.Write(meuByte);
+            Console.Write(" ");
+        }
+    }
+}
